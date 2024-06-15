808a39b0f3dd7c85654955f4aed8e73d2955dfca
Thomas Kabassis <tkabassis@gmail.com>
2024-06-15 20:45:18 +0100

    Initial service

---
+++ src/main/java/com/bc/pagewatch/Application.java
@@ -0,0 +1,13 @@
+package com.bc.pagewatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PageWatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PageWatchApplication.class, args);
+	}
+
+}

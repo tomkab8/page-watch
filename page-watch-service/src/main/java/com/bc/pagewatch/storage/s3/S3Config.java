7d8f2776d0c2709ba46b0925088aba02b19974a7
Thomas Kabassis <tkabassis@gmail.com>
2024-08-27 15:40:10 +0100

    Correct reading of S3 storage properties

--- page-watch-service/src/main/java/com/bc/pagewatch/storage/s3/S3Config.java
+++ page-watch-service/src/main/java/com/bc/pagewatch/storage/s3/S3Config.java
@@ -5,9 +5,11 @@ import com.amazonaws.auth.AnonymousAWSCredentials;
 import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.bc.pagewatch.logging.LogBuilder;
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -15,24 +17,20 @@ import static com.amazonaws.regions.Regions.EU_CENTRAL_1;
 
 @Configuration
 @Log4j2
-//@EnableConfigurationProperties(S3Properties.class)
-public class S3Config {
+@AllArgsConstructor
+@EnableConfigurationProperties(S3Properties.class)
+public class S3Config implements InitializingBean {
 
-    @Value("${storage.s3.url}")
-    private String url;
+    private S3Properties s3Properties;
 
-    @Value("${storage.s3.bucket}")
-    private String bucket;
-
-    //private S3Properties s3Properties;
-    //@Override
-    //public void afterPropertiesSet() {
-    //    log.info(LogBuilder.configure(s3Properties));
-    //}
+    @Override
+    public void afterPropertiesSet() {
+        log.info(LogBuilder.configure(s3Properties));
+    }
 
     @Bean
-    public S3Service s3Service(S3Storage s3Storage) {
-        return new S3Service(s3Storage, bucket);
+    public S3Service s3Service(S3Properties properties, S3Storage s3Storage) {
+        return new S3Service(s3Storage, properties.getBucketName());
     }
 
     @Bean
@@ -41,11 +39,10 @@ public class S3Config {
     }
 
     @Bean
-    public AmazonS3 s3Client() {
+    public AmazonS3 s3Client(S3Properties properties) {
         return AmazonS3ClientBuilder
                 .standard()
-//                .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(properties.getUrl(), EU_CENTRAL_1.getName()))
-                .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(url, EU_CENTRAL_1.getName()))
+                .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(properties.getUrl(), EU_CENTRAL_1.getName()))
                 .withPathStyleAccessEnabled(true)
 // .withClientConfiguration(new ClientConfiguration())
                 .withCredentials(new AWSStaticCredentialsProvider(new AnonymousAWSCredentials()))
--- page-watch-service/src/main/java/com/bc/pagewatch/storage/s3/S3Properties.java
+++ page-watch-service/src/main/java/com/bc/pagewatch/storage/s3/S3Properties.java
@@ -1,17 +1,17 @@
 package com.bc.pagewatch.storage.s3;
 
-import lombok.Getter;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties(prefix = "storage.s3")
 @NoArgsConstructor
-@Getter
+@Data
 public class S3Properties {
 
     private String url;
 
-    private String bucket;
+    private String bucketName;
 
     private String accessKey;
 

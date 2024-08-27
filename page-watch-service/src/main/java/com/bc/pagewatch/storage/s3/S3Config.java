d9deb7e2183f447bbe7b2bfe08390ea6ea5660f6
Thomas Kabassis <tkabassis@gmail.com>
2024-08-27 15:28:50 +0100

    Fix multi-module build structure

--- page-watch-service/src/main/java/com/bc/pagewatch/storage/s3/S3Config.java
+++ page-watch-service/src/main/java/com/bc/pagewatch/storage/s3/S3Config.java
@@ -5,12 +5,9 @@ import com.amazonaws.auth.AnonymousAWSCredentials;
 import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-import com.bc.pagewatch.logging.LogBuilder;
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.apache.logging.log4j.core.layout.AbstractStringLayout;
-import org.springframework.beans.factory.InitializingBean;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -18,20 +15,24 @@ import static com.amazonaws.regions.Regions.EU_CENTRAL_1;
 
 @Configuration
 @Log4j2
-@AllArgsConstructor
-@EnableConfigurationProperties(S3Properties.class)
-public class S3Config implements InitializingBean {
+//@EnableConfigurationProperties(S3Properties.class)
+public class S3Config {
 
-    private S3Properties s3Properties;
+    @Value("${storage.s3.url}")
+    private String url;
 
-    @Override
-    public void afterPropertiesSet() {
-        log.info(LogBuilder.configure(s3Properties));
-    }
+    @Value("${storage.s3.bucket}")
+    private String bucket;
+
+    //private S3Properties s3Properties;
+    //@Override
+    //public void afterPropertiesSet() {
+    //    log.info(LogBuilder.configure(s3Properties));
+    //}
 
     @Bean
-    public S3Service s3Service(S3Properties s3Properties, S3Storage s3Storage) {
-        return new S3Service(s3Storage, s3Properties.getBucket());
+    public S3Service s3Service(S3Storage s3Storage) {
+        return new S3Service(s3Storage, bucket);
     }
 
     @Bean
@@ -40,11 +41,11 @@ public class S3Config implements InitializingBean {
     }
 
     @Bean
-    public AmazonS3 s3Client(S3Properties properties) {
+    public AmazonS3 s3Client() {
         return AmazonS3ClientBuilder
                 .standard()
 //                .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(properties.getUrl(), EU_CENTRAL_1.getName()))
-                .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration("http://localhost:4566", EU_CENTRAL_1.getName()))
+                .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(url, EU_CENTRAL_1.getName()))
                 .withPathStyleAccessEnabled(true)
 // .withClientConfiguration(new ClientConfiguration())
                 .withCredentials(new AWSStaticCredentialsProvider(new AnonymousAWSCredentials()))
